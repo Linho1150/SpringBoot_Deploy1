2782816212fb531569da31e7b4cb378cc9edc8f8
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2022-07-01 21:25:07 +0900

    chros: CD 확인을 위한 커멘트

--- src/main/java/com/example/helloworld/HelloworldController.java
+++ src/main/java/com/example/helloworld/HelloworldController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HelloworldController {
-
     @RequestMapping()
     @GetMapping()
     public String main(){
         return "Hello World";
     }
+    //CheckCD
 }
